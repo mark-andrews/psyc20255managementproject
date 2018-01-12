9e54ce95e8398e681663b05d5b9b9f2a6843ef0c
Mark Andrews <mjandrews.net@gmail.com>
2018-01-12 01:48:38 +0000

    Change some conf settings
    * Stop using commas in file names
    * Add file of the marking template

--- psyc20255management/conf.py
+++ psyc20255management/conf.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 # These are the UG grades. These probably won't change.
 grades = '''
@@ -20,11 +21,19 @@ labgroups = [str(i+1) + ' SH' for i in range(6)] +\
 submitted_report_filename_pattern\
     = re.compile(r'([0-9]{5,6}-[0-9]{5}) - ([NT]0[0-9]{6}) - (.*[a-z])- ([^-]*) - (.*)')
 
+# The template document for the marksheets
+this_dir = os.path.dirname(os.path.abspath(__file__))
+marksheet_template_fname\
+        = os.path.abspath(os.path.join(this_dir,
+            'marksheet_templates/psyc20255_marksheet_template_v0.docx')
+            )
+
+
 # When we create copies of the submitted reports, we will use the following
 # file name template.
-new_report_fname_template = '%s, %s, report%s'
+new_report_fname_template = '%s__%s__%s__report%s'
 
 # Marksheets have the following file name template.
-marksheet_fname_template = '%s, %s, marksheet.docx'
+marksheet_fname_template = '%s__%s__%s__marksheet.docx'
 # And here's the regex.
-marksheet_fname_pattern = re.compile(r'([^,]*), ([^,]*), marksheet.docx')
+marksheet_fname_pattern = re.compile(r'(.*)__(.*)__(.*)__marksheet.docx')
